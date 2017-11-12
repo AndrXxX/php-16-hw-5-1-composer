ae2c9cd92a1c9054a3dc2c6c3c2091f1a9e4fdfa
Unknown <andrew1gar@yandex.ru>
2017-11-12 17:38:45 +0300

    ver 0.1

---
+++ core/core.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'functions.php';
+require "./vendor/autoload.php";
+
+session_start();
+
+$homeWorkNum = '5.1';
+$homeWorkCaption = 'Менеджер зависимостей Composer.';
+
+/**
+ * Устанавливаем системные настройки
+ */
+ini_set("display_errors", "1"); // Показ ошибок
+ini_set("display_startup_errors", "1");
+ini_set('error_reporting', E_ALL);
+ini_set('safe_mode', false);
+ini_set('open_basedir', NULL);
+mb_internal_encoding('UTF-8'); // Кодировка по умолчанию
+
+/**
+ * Загружаем классы
+ */
+spl_autoload_register(function($name) {
+    $file = dirname(__DIR__) . '/core/' . $name . '.class.php';
+    if (!file_exists($file)) {
+        throw new Exception('Autoload class: File ' . $file . ' not found');
+    }
+    require $file;
+});
